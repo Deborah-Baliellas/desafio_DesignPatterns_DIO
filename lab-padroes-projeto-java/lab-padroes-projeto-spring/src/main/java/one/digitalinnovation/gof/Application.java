9f8014f8a5a4a8e385054c516d19062ce9ab9338
Deborah Baliellas <dbaliellas@gmail.com>
2024-07-29 20:52:37 -0300

    projeto

---
+++ lab-padroes-projeto-java/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation.gof;
+
+@EnableFeignClients
+@SpringBootApplication
+
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
